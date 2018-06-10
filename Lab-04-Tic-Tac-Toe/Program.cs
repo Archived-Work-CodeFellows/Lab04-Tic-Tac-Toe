d113c00222669903ae53a8ea34f66597d36ad625
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-09 20:30:53 -0700

    Created base project for lab and unit tests

---
+++ Lab-04-Tic-Tac-Toe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_04_Tic_Tac_Toe
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tic-Tac-Toe-Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Lab_04_Tic_Tac_Toe;
+
+namespace Tic_Tac_Toe_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
