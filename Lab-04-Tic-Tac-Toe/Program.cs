ec64e8f0826b68865bd717f3b0ebff8e4734d33f
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-10 19:19:06 -0700

    Added player creation and default board initialization

--- Lab-04-Tic-Tac-Toe/Program.cs
+++ Lab-04-Tic-Tac-Toe/Program.cs
@@ -7,14 +7,40 @@ namespace Lab_04_Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");         
+            Console.WriteLine("Hello World!");
+            MainMenu();
+        }
+
+        static void MainMenu()
+        {
+            Console.WriteLine("Welcome to Tic-Tac-Toe, Three in a row!");
+            Console.Write("\n\n");
+            Console.WriteLine("Start a new game? yes/no");
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "yes" || input == "y")
+            {
+                GamePlay();
+            }
+            Console.Clear();
+            Console.WriteLine("Okay maybe next time! bye!");
         }
 
         static void GamePlay()
         {
+            Console.Clear();
             GameBoard board = new GameBoard();
+
+            Console.WriteLine("Alright, player X, what is your name?");
+            string num1 = Console.ReadLine();
+            Player player1 = new Player(num1, "X");
+
+            Console.WriteLine("Player O, it is your turn.?");
+            string num2 = Console.ReadLine();
+            Player player2 = new Player(num2, "O");
+
             board.BoardDisplay(" ", " ");
-            
+
         }
     }
 }
